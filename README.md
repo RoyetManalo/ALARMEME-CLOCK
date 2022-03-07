d69898365a14ca907d653a5bc0674c3e10e31d6d
Royet Manalo <royetmanalo@gmail.com>
2022-03-07 18:54:32 +0800

    Added readme

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
-# alarm-clock
+# Alarmeme Clock
+
+> A Personal Project
+
+## Description
+
+> An alarm clock that shows dancing memes when alarms fires off
+
+### Live Demo Here
+
+[Alarmeme Clock](https://alarmeme-clock.netlify.app/)
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,50 +1,49 @@
 <template>
-   <footer>
-        <p>This is a simple project by Royet Manalo</p>
-        <p>The goal of this project is to improve my VUE Javascript skills and knowledge</p>
-        <p>Copyright &copy; 2021</p>
-   </footer>
-    
+  <footer>
+    <p>This is a simple project by Royet Manalo</p>
+    <p>
+      The goal of this project is to improve my VUE Javascript skills and
+      knowledge
+    </p>
+    <p>Copyright &copy; 2021</p>
+  </footer>
 </template>
 
-
 <script>
-    export default {
-        name: 'Footer',
-        components: {
-           
-        }
-    }
+export default {
+  name: "Footer",
+  components: {},
+};
 </script>
 
-
 <style scoped>
-    footer{
-        font-family: "Kiwi Maru", serif;
-        font-size: 12px;
-        color: #fff;
-        text-align: center;
-        position: fixed;
-        bottom: 0;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        line-height: 1.5;
-        z-index: 5;
-        
-    }
+footer {
+  font-family: "Kiwi Maru", serif;
+  font-size: 12px;
+  color: #fff;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  line-height: 1.5;
+  z-index: 5;
+}
 
-    footer::before {
-        content: "";
-        position: absolute;
-        background: black;
-        border-radius: 10px;
-        opacity: 0.5;
-        z-index: -1;
-        width: 500px;
-        height: 70px;
-        bottom: 0;
-        left: 50%;
-        transform: translate(-50%, 10%);
-    } 
-  
+footer::before {
+  content: "";
+  position: absolute;
+  background: black;
+  border-radius: 10px;
+  opacity: 0.5;
+  z-index: -1;
+  width: 500px;
+  height: 70px;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 10%);
+}
+
+@media (max-height: 667) {
+}
 </style>
